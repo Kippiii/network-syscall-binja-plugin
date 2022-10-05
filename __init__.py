7c6fcc4aeb7d1811f76315fcc640c6deba0b0107
Kippiii <ianorzel@gmail.com>
2022-10-04 22:28:32 -0400

    Adding a function to get all syscall instructions

--- __init__.py
+++ __init__.py
@@ -1,4 +1,22 @@
+from typing import List
+
 from binaryninja.plugin import PluginCommand
+from binaryninja import LowLevelILOperation, LowLevelILInstruction
+
+class Syscall:
+    num: int
+    args: List[int]
+    def __init__(self, inst):
+        pass
+
+def get_syscall_instructions(bv) -> List[LowLevelILInstruction]:
+    insts = []
+    for function in bv.functions:
+        for block in function.low_level_il:
+            for i in block:
+                if (i.operation == LowLevelILOperation.LLIL_SYSCALL):
+                    insts.append(i)
+    return insts
 
 def update_syscalls(bv):
     pass
