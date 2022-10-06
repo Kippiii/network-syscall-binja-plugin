72872d499c9385e02f259e16c15f05619d9f0e57
Kippiii <ianorzel@gmail.com>
2022-10-05 20:53:48 -0400

    Testing using all the different syscall classes

--- __init__.py
+++ __init__.py
@@ -1,27 +1,51 @@
-from typing import List
+from typing import List, Tuple
 
 from binaryninja.plugin import PluginCommand
-from binaryninja import LowLevelILOperation, LowLevelILInstruction
+from binaryninja import LowLevelILOperation, LowLevelILInstruction, RegisterValue
 
 
 class NotNetworkSyscallException(Exception):
     pass
 
 
+def parse_sockaddr(value: int) -> Tuple[int, int, str]:
+    family = value & 0xf
+    port = (value & 0xff00) >> 8
+    ip = f"{(value & 0xff0000) >> 16}.{(value & 0xff000000) >> 24}.{(value & 0xff00000000) >> 32}.{(value & 0xff0000000000) >> 40}"
+    return family, port, ip
+
+
 class Syscall:
-    num: int
+    name: str
     args: List[int]
     addr: int
 
     @classmethod
     def create(cls, inst: LowLevelILInstruction) -> "Syscall":
-        return Syscall(inst.address)
+        syscall_map = {
+            41: SocketSyscall,
+            49: BindSyscall,
+            50: ListenSyscall,
+            43: AcceptSyscall,
+            42: ConnectSyscall,
+            46: SendSyscall,
+            47: RecvSyscall,
+        }
+        sys_num = inst.get_reg_value("rax").value
+        if sys_num not in syscall_map:
+            raise NotNetworkSyscallException
+        regs = ["rdi", "rsi", "edx", "r10", "r8", "r9"]
+        args = [inst.get_reg_value(reg) for reg in regs]
+        syscall = syscall_map[sys_num](args)
+        syscall.addr = inst.address
+        return syscall
 
-    def __init__(self, addr: int):
-        self.addr = addr
+    def __init__(self):
+        pass
 
     def __str__(self) -> str:
-        return "This is a comment"
+        params = ", ".join(map(str, self.args))
+        return f"{self.name}({params})"
 
     def add_comment(self, bv) -> None:
         bv.set_comment_at(self.addr, str(self))
@@ -32,27 +56,59 @@ class SocketSyscall(Syscall):
     type: int
     protocol: int
 
+    def __init__(self, args: List[RegisterValue]) -> None:
+        self.domain = args[0].value
+        self.type = args[1].value
+        self.protocol = args[2].value
+
+    def __str__(self) -> str:
+        return f"socket({self.domain}, {self.type}, {self.protocol})"
+
 
 class BindSyscall(Syscall):
+    family: int
     ip_addr: str
     port: int
-    sock_addr_len: int
+
+    def __init__(self, args: List[RegisterValue]) -> None:
+        self.family, self.port, self.ip_addr = parse_sockaddr(args[1].value)
+
+    def __str__(self) -> str:
+        return f"bind({self.family}, {self.port}, {self.ip_addr})"
 
 
 class ListenSyscall(Syscall):
     backlog: int
 
+    def __init__(self, args: List[RegisterValue]) -> None:
+        self.backlog = args[1].value
+
+    def __str__(self) -> str:
+        return f"listen({self.backlog})"
+
 
 class AcceptSyscall(Syscall):
+    family: int
     ip_addr: str
     port: int
-    sock_addr_len: int
+
+    def __init__(self, args: List[RegisterValue]) -> None:
+        self.family, self.port, self.ip_addr = parse_sockaddr(args[1].value)
+
+    def __str__(self) -> str:
+        return f"accept({self.family}, {self.port}, {self.ip_addr})"
 
 
 class ConnectSyscall(Syscall):
+    family: int
     ip_addr: str
     port: int
-    sock_addr_len: int
+
+    def __init__(self, args: List[RegisterValue]) -> None:
+        self.family, self.port, self.ip_addr = parse_sockaddr(args[1].value)
+    
+    def __str__(self) -> str:
+        return f"connect({self.family}, {self.port}, {self.ip_addr})"
 
 
 class SendSyscall(Syscall):
@@ -61,12 +117,24 @@ class SendSyscall(Syscall):
     data: str
     flags: int
 
+    def __init__(self, args: List[RegisterValue]) -> None:
+        pass
+    
+    def __str__(self) -> str:
+        return "send syscall has not been configured"
+
 
 class RecvSyscall(Syscall):
     ip_addr: str
     port: int
     data: str
     flags: int
+
+    def __init__(self, args: List[RegisterValue]) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return "recv syscall has not been configured"
 
 
 def get_syscall_instructions(bv) -> List[LowLevelILInstruction]:
