8f006843fe1679fbec748e187806c70b2fa31e1b
Kippiii <ianorzel@gmail.com>
2022-10-16 14:52:58 -0400

    Adding docstrings to all functions and classes

--- __init__.py
+++ __init__.py
@@ -9,6 +9,14 @@ class NotNetworkSyscallException(Exception):
 
 
 def get_reg_value(inst: LowLevelILInstruction, reg_name: str, size: int = 8) -> Optional[int]:
+    """
+    Gets the value for a syscall parameter
+
+    :param inst: The instruction where the syscall is made
+    :param reg_name: The name of the register where parameter is stored
+    :param size: The size of the parameter (only used when on stack)
+    :return: The value of the parameter (or None if cannot be found)
+    """
     reg = inst.get_reg_value(reg_name)
     if reg.type == 0:
         return None
@@ -20,6 +28,12 @@ def get_reg_value(inst: LowLevelILInstruction, reg_name: str, size: int = 8) -> 
 
 
 def parse_sockaddr(value: Optional[int]) -> Tuple[int, int, str]:
+    """
+    Parses the sockaddr struct
+
+    :param value: The value of the struct
+    :return: A tuple of the family, port, and address
+    """
     if value is None:
         return -1, -1, ""
     family = value & 0xf
@@ -30,12 +44,20 @@ def parse_sockaddr(value: Optional[int]) -> Tuple[int, int, str]:
 
 
 class Syscall:
-    name: str
-    args: List[int]
+    """
+    Stores a syscall object
+
+    :param addr: The address where the syscall is made
+    """
     addr: int
 
     @classmethod
     def create(cls, inst: LowLevelILInstruction) -> "Syscall":
+        """
+        Creates a syscall
+
+        :param inst: The instruction of the syscall
+        """
         syscall_map = {
             41: SocketSyscall,
             49: BindSyscall,
@@ -55,15 +77,23 @@ class Syscall:
     def __init__(self):
         pass
 
-    def __str__(self) -> str:
-        params = ", ".join(map(str, self.args))
-        return f"{self.name}({params})"
-
     def add_comment(self, bv) -> None:
+        """
+        Adds the comment for a syscall
+
+        :param bv: The binary ninja object
+        """
         bv.set_comment_at(self.addr, str(self))
 
 
 class SocketSyscall(Syscall):
+    """
+    Represents an instance of the socket syscall
+
+    :param domain: The domain used to create the socket
+    :param type: The type used to create the socket
+    :param protocol: The protocol used to create the socket
+    """
     domain: Optional[int]
     type: Optional[int]
     protocol: Optional[int]
@@ -74,6 +104,11 @@ class SocketSyscall(Syscall):
         self.protocol = get_reg_value(inst, "rdx")
 
     def __str__(self) -> str:
+        """
+        Creates the string for the socket syscall
+
+        :return: The string used for the comment
+        """
         if self.protocol != 0 or self.type not in (1, 2):
             return "Created Socket of Unknown Type"
         network_proc = "TCP" if self.type == 1 else "UDP"
@@ -81,6 +116,13 @@ class SocketSyscall(Syscall):
 
 
 class BindSyscall(Syscall):
+    """
+    Represents an instance of the bind syscall
+
+    :param family: The family of the address
+    :param ip_addr: The ip address bound to
+    :param port: The port bound to
+    """
     family: int
     ip_addr: str
     port: int
@@ -94,21 +136,43 @@ class BindSyscall(Syscall):
         self.family, self.port, self.ip_addr = parse_sockaddr(value)
 
     def __str__(self) -> str:
+        """
+        Creates the string for the bind syscall
+
+        :return: The string used for the comment
+        """
         port_str = self.port if self.port != -1 else "Unknown"
         return f"Bound to Port {port_str}"
 
 
 class ListenSyscall(Syscall):
+    """
+    Represents an instance of the listen syscall
+
+    :param backlog: The backlog for listening
+    """
     backlog: Optional[int]
 
     def __init__(self, inst: LowLevelILInstruction) -> None:
         self.backlog = get_reg_value(inst, "rsi", 4)
 
     def __str__(self) -> str:
+        """
+        Creates the string for the listen syscall
+
+        :return: The string used for the comment
+        """
         return f"Listening with Backlog={self.backlog if self.backlog is not None else 'Unknown'}"
 
 
 class AcceptSyscall(Syscall):
+    """
+    Represents an instance of the accept syscall
+
+    :param family: The family accepting on
+    :param ip_addr: The ip address accepting on
+    :param port: The port accepting on
+    """
     family: int
     ip_addr: str
     port: int
@@ -119,11 +183,23 @@ class AcceptSyscall(Syscall):
         self.family, self.port, self.ip_addr = parse_sockaddr(value)
 
     def __str__(self) -> str:
+        """
+        Creates the string for the accept syscall
+
+        :return: The string used for the comment
+        """
         port_str = self.port if self.port != -1 else "Unknown"
         return f"Accepting on Port {port_str}"
 
 
 class ConnectSyscall(Syscall):
+    """
+    Represents an instance of the connect syscall
+
+    :param family: The family connecting on
+    :param ip_addr: The ip connecting to
+    :param port: The port connecting to
+    """
     family: int
     ip_addr: str
     port: int
@@ -134,11 +210,24 @@ class ConnectSyscall(Syscall):
         self.family, self.port, self.ip_addr = parse_sockaddr(value)
     
     def __str__(self) -> str:
+        """
+        Creates the string for the connect syscall
+
+        :return: The string used for the comment
+        """
         port_str = self.port if self.port != -1 else "Unknown"
         return f"Connecting on Port {port_str}"
 
 
 class SendToSyscall(Syscall):
+    """
+    Represents an instance of the sendto syscall
+
+    :param family: The family being sent on
+    :param ip_addr: The ip address being sent to
+    :param port: The port being sent to
+    :param data: The data being sent
+    """
     family: int
     ip_addr: str
     port: int
@@ -153,12 +242,24 @@ class SendToSyscall(Syscall):
         self.data = ''.join([chr((buffer // (16**i)) % 16) for i in range(buffer_size)])[::-1] if buffer is not None else None
     
     def __str__(self) -> str:
+        """
+        Creates the string for the sendto syscall
+
+        :return: The string used for the comment
+        """
         port_str = self.port if self.port != -1 else "Unknown"
         data_str = self.data if self.data is not None else "Unknown"
         return f"Sent '{data_str}' on Port {port_str}"
 
 
 class RecvFromSyscall(Syscall):
+    """
+    Represents an instance of the recvfrom syscall
+
+    :param family: The family being used for recieving
+    :param ip_addr: The ip address being recieved from
+    :param port: The port being recieved from
+    """
     family: int
     ip_addr: str
     port: int
@@ -169,11 +270,22 @@ class RecvFromSyscall(Syscall):
         self.family, self.port, self.ip_addr = parse_sockaddr(value)
 
     def __str__(self) -> str:
+        """
+        Creates the string for the recvfrom syscall
+
+        :return: The string used for the comment
+        """
         port_str = self.port if self.port != -1 else "Unknown"
         return f"Recieved Data on Port {port_str}"
 
 
 def get_syscall_instructions(bv) -> List[LowLevelILInstruction]:
+    """
+    Gets a list of instruction where syscalls occur
+
+    :param bv: The binary ninja object
+    :return: The list of instructions
+    """
     insts = []
     for function in bv.functions:
         for block in function.low_level_il:
@@ -184,6 +296,12 @@ def get_syscall_instructions(bv) -> List[LowLevelILInstruction]:
 
 
 def gen_syscalls(insts: List[LowLevelILInstruction]) -> List[Syscall]:
+    """
+    Generates syscalls objects at all instructions
+
+    :param insts: The instructions where syscalls occur
+    :return: A list of syscall objects
+    """
     syscalls = []
     for i in insts:
         try:
@@ -194,6 +312,11 @@ def gen_syscalls(insts: List[LowLevelILInstruction]) -> List[Syscall]:
 
 
 def update_syscalls(bv):
+    """
+    Adds comments to a program at all the network syscall instructions
+
+    :param bv: The binary ninja object
+    """
     insts = get_syscall_instructions(bv)
 
     syscalls = gen_syscalls(insts)
